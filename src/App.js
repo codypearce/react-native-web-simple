ff72315564bb807695f62e76840597d88dce6708
codypearce <codyapearce@gmail.com>
2019-08-14 08:28:44 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// component.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import { Text } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return <Text>Hello world</Text>;
+  }
+}
